e47122c2f655f4b8688c7de3e3abb446ed6dde05
Piero Giubilato <giubilat@cern.ch>
2022-05-06 18:52:55 +0200

    PWM module and TB added

---
+++ sources/simulation/pwm_tb.sv
@@ -0,0 +1,101 @@
+/*#############################################################################\
+##                                                                            ##
+##       APPLIED ELECTRONICS - Physics Department - University of Padova      ##
+##                                                                            ## 
+##       ---------------------------------------------------------------      ##
+##                                                                            ##
+##             Sigma Delta Analogue to Digital didactical example             ##
+##                                                                            ##
+\#############################################################################*/
+
+
+// Set timescale (default time unit if not otherwise specified).
+`timescale 1ns / 1ps
+
+// Define Module for Test Fixture
+module pwm_tb ();
+
+    // ==========================================================================
+    // ==                               Parameters                             ==
+    // ==========================================================================
+    
+    // Timing properties.
+    parameter C_CLK_FRQ         = 100000000;    // Main clock frequency [Hz].
+    parameter C_CLK_JTR         = 50;           // Main clock jitter [ps].
+    localparam real C_CLK_PERIOD = 1E9 / C_CLK_FRQ;  // Master clock period [ns].
+        
+    // PWM propeties.
+    parameter C_LEVEL_WIDTH     = 8;            // PWM range width.
+
+    
+    // ==========================================================================
+    // ==                              Seeding                                 ==
+    // ==========================================================================
+    
+    // Seeding for (repeatable) random number generation.
+    static int seed = $urandom + 0;
+
+
+    // ==========================================================================
+    // ==                                Signals                               ==
+    // ==========================================================================
+    
+    // Timing signal.
+    reg rRstb;
+    reg rClk;
+    
+    // Data in.
+    reg [C_LEVEL_WIDTH - 1 : 0] rLevel;
+
+    // DAta out.
+    wire wOut;
+
+
+
+    // ==========================================================================
+    // ==                                 DUTs                                 ==
+    // ==========================================================================
+
+    // Instantiate the UUT
+    debounce #(
+        .C_CLK_FRQ(C_CLK_FRQ),
+        .C_LEVEL_WIDTH(C_LEVEL_WIDTH)
+    ) DUT (
+        .rstb(rRstb), 
+        .clk(rClk), 
+        .level(rLevel), 
+        .out(wOut)
+    );
+
+
+    // Initialize Inputs
+    initial begin
+		$display ($time, " << Starting the Simulation >> ");
+        rRstb = 1'b0;
+		rClk = 1'b0;
+        #200 rRstb = 1'b1;
+        rLevel = 1'b0;
+    end
+
+    // Main clock generation. This process generates a clock with period equal to 
+    // C_CLK_PERIOD. It also add a pseudorandom jitter, normally distributed 
+    // with mean 0 and standard deviation equal to 'kClockJitter'.  
+    always begin
+        #(0.001 * $dist_normal(seed, 1000.0 * C_CLK_PERIOD / 2, C_CLK_JTR));
+        rClk = ! rClk;
+    end  
+	
+
+    // Pseudosequence.
+	always begin
+		
+        for (int i = 0; i <= C_LEVEL_WIDTH; i++) begin
+            rLevel = 2**i - 1;
+            #100us; 
+		end
+		
+		// Stop simulation.
+        $finish;
+	end
+
+endmodule
---
+++ sources/synthesys/pwm.v
@@ -0,0 +1,85 @@
+/*#############################################################################\
+##                                                                            ##
+##       APPLIED ELECTRONICS - Physics Department - University of Padova      ##
+##                                                                            ## 
+##       ---------------------------------------------------------------      ##
+##                                                                            ##
+##             Sigma Delta Analogue to Digital didactical example             ##
+##                                                                            ##
+\#############################################################################*/
+
+// INFO
+// The pwm module generates a pulse train which duty cycle corresponds to the
+// input 'level' value. The 'C_MIN_PULSE' defines the minimum duration of a
+// single pulse.
+
+
+// -----------------------------------------------------------------------------
+// --                                PARAMETERS                               --
+// -----------------------------------------------------------------------------
+//
+// C_CLK_FRQ:       frequency of the clock in [cycles per second] {100000000}. 
+// C_MIN_PULSE:     the time interval the signal must be stable to pass through
+//					and reach the fabric. [ms] {10}.
+// C_LEVEL_WIDTH:   the 'level' width, indicating the number of bits used to
+//                  calculate the pulse duty cycle.
+
+// -----------------------------------------------------------------------------
+// --                                I/O PORTS                                --
+// -----------------------------------------------------------------------------
+//
+// rstb:            INPUT, synchronous reset, ACTIVE LOW. 
+// clk:             INPUT, master clock.
+// level:          	INPUT, the signal indicating the duty cycle. The signal duty
+//                  cycle will result equal to 'level / (2^C_LEVEL_WIDTH - 1)'.
+// out:           	OUTPUT: the pulsed signal.
+
+
+// Tool timescale.
+`timescale 1 ns / 1 ps
+
+// Behavioural.
+module  debounce # (
+		parameter C_CLK_FRQ = 100000000,  	// Clock frequency [Hz].
+		parameter C_MIN_PULSE = 1000,       // Minimum pulse width [ns].
+        parameter C_LEVEL_WIDTH = 8         // Level range.        
+	) (
+		input rstb,
+		input clk,
+		input [C_LEVEL_WIDTH-1 : 0] level,  // The modulation level.
+		output reg out		                // The modulated output.
+	);  
+
+
+   	// =========================================================================
+    // ==                        Registers and wires                          ==
+    // =========================================================================
+
+	// Counters.
+	reg [C_LEVEL_WIDTH - 1 : 0] rCount;
+	
+
+	// =========================================================================
+    // ==                        Synchronous counters                         ==
+    // =========================================================================
+
+	// Increments the counter if the signal is stable
+	always @ (posedge clk) begin
+		
+		// Reset or count.
+    	rCount <= (rstb ==  1'b0) ? { C_LEVEL_WIDTH {1'b0} } : rCount + 1;
+    end
+
+
+	// =========================================================================
+    // ==                        Synchronous outputs                          ==
+    // =========================================================================
+
+	// Set the output high for 'level' clock cycles.
+	always @ (posedge clk) begin
+		out <= (rCount <= level) ? 1'b1 : 1'b0;
+	end
+
+endmodule
+
+
